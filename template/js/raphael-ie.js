6a7aafba36190ab13de33528172cf19a0753d8ee
Andrei Bosco <andreibosco@gmail.com>
2014-02-26 13:47:53 -0300

    - adicionando svg com texto; - mouseover em texto destacando elemento do desenho; - removendo propriedades do css pois causa conflito com propriedades passadas pelo raphaelJS;

--- template/js/raphael-ie.js
+++ template/js/raphael-ie.js
@@ -7,9 +7,11 @@ var path18125 = rsr.path("m 198.63539,598.05619 0,-186.02363 -157.647134,0 0,-17
 var path18129 = rsr.path("m 189.17656,598.05619 0,116.65888 104.04712,0").attr({id: 'path18129',parent: 'layer1',fill: 'none',stroke: '#000000',"stroke-width": '1',"stroke-miterlimit": '4',"stroke-dasharray": 'none','stroke-opacity': '1'}).transform("t-3.7890624e-6,-187.23976").data('id', 'path18129');
 var path18131 = rsr.path("m 293.22368,705.25624 -94.58829,0 0,-107.20005").attr({id: 'path18131',parent: 'layer1',fill: 'none',stroke: '#000000',"stroke-width": '1',"stroke-miterlimit": '4',"stroke-dasharray": 'none','stroke-opacity': '1'}).transform("t-3.7890624e-6,-187.23976").data('id', 'path18131');
 var path18135 = rsr.path("m 0,194.47951 0,677.88269 684.18858,0 0,-280.6119 59.90591,0 0,-176.56479 -59.90591,0 0,-220.706 -684.18858,0 z").attr({id: 'path18135',parent: 'layer1',fill: 'none',stroke: '#000000',"stroke-width": '1',"stroke-miterlimit": '4',"stroke-dasharray": '--',"stroke-dashoffset": '0','stroke-opacity': '1'}).transform("t-3.7890624e-6,-187.23976").data('id', 'path18135');
+var rect18745 = rsr.rect(7.731246, 717.98578, 167.1882, 30.924984).attr({id: 'rect18745',x: '7.731246',y: '717.98578',parent: 'layer1',"stroke-width": '1',"stroke-miterlimit": '4',"stroke-dasharray": 'none',"stroke-dashoffset": '0',fill: '#ffffff',stroke: '#ff0000',"stroke-opacity": '1',"fill-opacity": '1'}).transform("t-3.7890624e-6,-187.23976 t3.7890624e-6,187.23976").data('id', 'rect18745');
 var text18697 = rsr.text(15.789299, 927.47614, 'janela alta').attr({id: 'text18697',parent: 'layer1',"font-size": '16px',"font-style": 'normal',"font-variant": 'normal',"font-weight": 'normal',"font-stretch": 'normal',"text-align": 'start',"line-height": '125%',"letter-spacing": '0px',"word-spacing": '0px',"writing-mode": 'lr-tb',"text-anchor": 'start',fill: '#000000',"fill-opacity": '1',stroke: 'none','stroke-width':'1','stroke-opacity':'1',"font-family": 'Sans',"-inkscape-font-specification": 'Sans'}).transform("t-3.7890624e-6,-187.23976").data('id', 'text18697');
 var text18701 = rsr.text(14.148674, 957.78827, 'folha de janela alta').attr({id: 'text18701',parent: 'layer1',"font-size": '16px',"font-style": 'normal',"font-variant": 'normal',"font-weight": 'normal',"font-stretch": 'normal',"text-align": 'start',"line-height": '125%',"letter-spacing": '0px',"word-spacing": '0px',"writing-mode": 'lr-tb',"text-anchor": 'start',fill: '#000000',"fill-opacity": '1',stroke: 'none','stroke-width':'1','stroke-opacity':'1',"font-family": 'Sans',"-inkscape-font-specification": 'Sans'}).transform("t-3.7890624e-6,-187.23976").data('id', 'text18701');
 path18135.node.id = 'coberta';
+rect18745.node.id = 'cobertaTxt';
 
 var hoverIn = function() {
     this.attr({"stroke": "#ff0000"});
@@ -20,4 +22,5 @@ var hoverOut = function() {
 }
 
 path18135.hover(hoverIn, hoverOut, path18135, path18135);
+rect18745.hover(hoverIn, hoverOut, path18135, path18135);
 text18697.hover(hoverIn, hoverOut, path18135, path18135);
