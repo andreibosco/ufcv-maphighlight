25fc806b93250f9938284c245a05944f7b96a085
Andrei Bosco <andreibosco@gmail.com>
2014-02-26 13:05:11 -0300

    - continuando testes com raphaelJS: gerando desenho original no autocad, convertendo em svg com o inkscape e usando um conversor online p/ o formato do RaphaelJS

---
+++ anotacoes/planta_baixa-itens.md
@@ -0,0 +1,24 @@
+# Azul
+- janela alta
+- folha da janela alta
+- janela baixa
+- folha janela baixa
+- projeção coberta
+
+# Vermelho
+- aresta de piso
+- parede baixa (peitoril)
+- desníveis
+- linha de extensão ou chamada
+- linha contínua fina (linhas de cota)
+
+# Verde
+- porta de correr
+- porta de abrir
+- linha corte
+- direção do corte
+- letra do corte (corte BB)
+
+# Preto
+- parede alta
+- pilar
---
+++ template/js/teste-inkscape.js
@@ -0,0 +1,22 @@
+var rsr = Raphael('rsr', '744.09448', '1052.3622');
+
+var layer1 = rsr.set();
+var rect16853 = rsr.rect(100, 135.21933, 560, 91.428574).attr({x: '100',y: '135.21933',id: 'rect16853',parent: 'layer1',fill: '#0000ff',"fill-rule": 'evenodd',stroke: '#000000',"stroke-width": '1',"stroke-linecap": 'butt',"stroke-linejoin": 'miter',"stroke-opacity": '1'}).data('id', 'rect16853');
+var path16855 = rsr.path("m 131.42857,360.93361 2.85714,382.85714").attr({id: 'path16855',parent: 'layer1',fill: 'none',stroke: '#000000',"stroke-width": '1',"stroke-linecap": 'butt',"stroke-linejoin": 'miter',"stroke-opacity": '1'}).data('id', 'path16855');
+var path16857 = rsr.path("m 380,409.50504 c 0,-15.2381 0,-15.2381 0,0 z").attr({id: 'path16857',parent: 'layer1',fill: 'none',stroke: '#000000',"stroke-width": '1',"stroke-linecap": 'butt',"stroke-linejoin": 'miter',"stroke-opacity": '1'}).data('id', 'path16857');
+var path16859 = rsr.path("M 71.428571,703.79075 411.42857,498.07647").attr({id: 'path16859',parent: 'layer1',fill: 'none',stroke: '#000000',"stroke-width": '1',"stroke-linecap": 'butt',"stroke-linejoin": 'miter',"stroke-opacity": '1'}).data('id', 'path16859');
+var path16865 = rsr.path("M 262.85714,378.07647 260,760.93361").attr({id: 'path16865',parent: 'layer1',fill: 'none',stroke: '#000000',"stroke-width": '1',"stroke-linecap": 'butt',"stroke-linejoin": 'miter',"stroke-opacity": '1'}).data('id', 'path16865');
+var path16867 = rsr.path("m 91.428571,915.21933 362.857139,-17.14286 -45.71428,-400").attr({id: 'path16867',parent: 'layer1',fill: 'none',stroke: '#000000',"stroke-width": '1',"stroke-linecap": 'butt',"stroke-linejoin": 'miter',"stroke-opacity": '1'}).data('id', 'path16867');
+layer1.attr({'id': 'layer1','name': 'layer1'});
+
+
+var rsrGroups = [layer1];
+layer1.push(
+	rect16853 ,
+	path16855 ,
+	path16857 ,
+	path16859 ,
+	path16865 ,
+	path16867 
+);
+	
